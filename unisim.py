8f0a10a714b0c4248e2e19bac350e59c18f96535
trav <devnull@localhost>
2013-01-02 23:51:44 -0600

    Added some operator overloading to vector3 (UNTESTED!)

--- unisim.py
+++ unisim.py
@@ -54,11 +54,19 @@ class Vector3:
         self.x += v.x
         self.y += v.y
         self.z += v.z
+        
+    #override +
+    def __add__(self, other):
+        return (Vector3(self.x+other.x, self.y+other.y, self.z+other.z))
 
     def sub(self, v):
         self.x -= v.x
         self.y -= v.y
         self.z -= v.z
+        
+    #override -
+    def __sub__(self, other):
+        return (Vector3(self.x-other.x, self.y-other.y, self.z-other.z))
         
     def dot(self, v):
         return self.x * v.x + self.y * v.y + self.z * v.z
