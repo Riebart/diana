d6f825b50e68a7a4e903738e31a705235167cfbf
trav <devnull@localhost>
2013-01-08 23:31:02 -0600

    Added a basic stress test

--- testor.py
+++ testor.py
@@ -6,6 +6,8 @@ import unisim
 import threading
 import ship
 from physics import Vector3
+import random
+import time
 
 def testSimple():
     miss1 = objectsim.Missile(osim)
@@ -68,13 +70,49 @@ def testShip():
     print "miss1 osimid is: %d" % miss1.osid
     print "miss1 unisim is: %d" % miss1.uniid    
     
+
+def rand_vec(rand, rang):
+    return Vector3((rand.random()*rang, rand.random()*rang, rand.random()*rang))
+
+
+def stressTest(ships=1000, area=10000):
     
+    for i in range(0,ships):
+        ship1 = ship.Ship(osim)
+        ship1.location = rand_vec(rand, area)
+        osim.spawn_object(ship1)
+        print "ship%d osimid is: %d" % (i, ship1.osid)
+        print "ship%d unisim is: %d" % (i, ship1.uniid)
+
+        direction = Vector3((1.0, 0.0,0.0))
+        miss1 = ship1.fire_missile(direction, 500)
+    
+        print "miss%d osimid is: %d" % (i, miss1.osid)
+        print "miss%d unisim is: %d" % (i, miss1.uniid)
 
 #unism.test()
 
+class TThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        
+    def run(self):
+        while True:
+            time.sleep(500)
+    
+
+def test_threads():
+    for i in range (0, 10000):
+        t = TThread()
+        t.start()
+        print "Started thread %d" % i
+
 osim = objectsim.ObjectSim()
+rand = random.Random()
+rand.seed(0)
 
 #testVisData()
 #testShip()
-testSimpleShip()
-
+#testSimpleShip()
+stressTest()
+#test_threads()
