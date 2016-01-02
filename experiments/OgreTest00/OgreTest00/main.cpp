0ba409f475b25b36e18d98d4153010c1ccc741df
Michael Himbeault <mike@riebart.ca>
2016-01-02 23:53:25 +0000

    Ogre3D example updated. On my machine it works, but the tutorials and documentation feel old.

--- experiments/OgreTest00/OgreTest00/main.cpp
+++ experiments/OgreTest00/OgreTest00/main.cpp
@@ -28,6 +28,18 @@
 #include <SdkCameraMan.h>
 #endif
 
+#include "BaseApplication.h"
+
+class TutorialApplication : public BaseApplication
+{
+public:
+    TutorialApplication(void);
+    virtual ~TutorialApplication(void);
+
+protected:
+    virtual void createScene(void);
+};
+
 #ifdef OGRE_STATIC_LIB
 #  define OGRE_STATIC_GL
 #  if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
@@ -49,7 +61,63 @@
 #  include "OgreStaticPluginLoader.h"
 #endif
 
-int main(int argc, char** argv)
+TutorialApplication::TutorialApplication(void)
 {
-    return 0;
 }
+//---------------------------------------------------------------------------
+TutorialApplication::~TutorialApplication(void)
+{
+}
+
+//---------------------------------------------------------------------------
+void TutorialApplication::createScene(void)
+{
+    mSceneMgr->setAmbientLight(Ogre::ColourValue(0.5, 0.5, 0.5));
+    Ogre::Entity* ogreEntity = mSceneMgr->createEntity("ogrehead.mesh");
+    Ogre::SceneNode* ogreNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+    ogreNode->attachObject(ogreEntity);
+    Ogre::Light* light = mSceneMgr->createLight("MainLight");
+    light->setPosition(20, 80, 50);
+}
+//---------------------------------------------------------------------------
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+#define WIN32_LEAN_AND_MEAN
+#include "windows.h"
+#endif
+
+#include <stdio.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+    INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT)
+#else
+    int main(int argc, char *argv[])
+#endif
+    {
+        printf("Running Ogre scene...\n");
+        // Create application object
+        TutorialApplication app;
+
+        try {
+            app.go();
+        }
+        catch (Ogre::Exception& e) {
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+            MessageBox(NULL, e.getFullDescription().c_str(), "An exception has occurred!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+#else
+            std::cerr << "An exception has occurred: " <<
+                e.getFullDescription().c_str() << std::endl;
+#endif
+        }
+
+        printf("Done\n");
+        return 0;
+    }
+
+#ifdef __cplusplus
+}
+#endif
