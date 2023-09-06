f345668c26ecb235b736838c5485404bdb807b62
Mike Himbeault <mike@forethought.solutions>
2023-09-05 20:52:50 -0500

    Committing new headers.

---
+++ unisim/unisim/src/__version.hpp
@@ -0,0 +1,6 @@
+#ifndef __VERSION_HPP
+#define __VERSION_HPP
+#define GIT_VERSION "experimental-ueclient-master-merge-2-geffe6c55"
+#define GIT_COMMIT_DATE "2023-09-06T01:51:57+00:00"
+#define BUILD_DATE "2023-09-06T01:52:32+00:00"
+#endif
