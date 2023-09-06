8a7e2a3afe98e1419aad000a4f272b8768c56419
Mike Himbeault <mike@forethought.solutions>
2023-09-06 17:38:17 -0500

    Updating binary

--- unisim/unisim/src/__version.hpp
+++ unisim/unisim/src/__version.hpp
@@ -1,7 +1,7 @@
 #ifndef __VERSION_HPP
 #define __VERSION_HPP
-#define GIT_VERSION "d4dae7b2"
+#define GIT_VERSION "cb725f74"
 #define GIT_BRANCH "master"
-#define GIT_COMMIT_DATE "2023-09-06T02:28:58+00:00"
-#define BUILD_DATE "2023-09-06T02:29:58+00:00"
+#define GIT_COMMIT_DATE "2023-09-06T22:35:31+00:00"
+#define BUILD_DATE "2023-09-06T22:35:44+00:00"
 #endif
