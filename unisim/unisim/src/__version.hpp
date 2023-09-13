2936e275eeb88609fcd5f3056274838f204092c0
Diana Actions <diana-actions@flyingfortressit.ca>
2023-09-13 03:51:44 +0000

    ACTION: Udating headers unisim headers

--- unisim/unisim/src/__version.hpp
+++ unisim/unisim/src/__version.hpp
@@ -1,7 +1,7 @@
 #ifndef __VERSION_HPP
 #define __VERSION_HPP
-#define GIT_VERSION "4a77020a"
+#define GIT_VERSION "2bf499a4"
 #define GIT_BRANCH "14-add-github-actions-for-building-headers-and-binaries"
-#define GIT_COMMIT_DATE "2023-09-13T03:35:15+00:00"
-#define BUILD_DATE "2023-09-13T03:35:43+00:00"
+#define GIT_COMMIT_DATE "2023-09-13T03:51:23+00:00"
+#define BUILD_DATE "2023-09-13T03:51:43+00:00"
 #endif
