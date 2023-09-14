2891b35f317802858b0a3c9119ae3c7beb98e8e6
Diana Actions <diana-actions@flyingfortressit.ca>
2023-09-14 03:47:47 +0000

    ACTION: Updating unisim headers and binaries

--- unisim/unisim/src/include/__version.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef __VERSION_HPP
-#define __VERSION_HPP
-#define GIT_VERSION "7d65fa84"
-#define GIT_BRANCH "54/change-default-to-realtime-physics"
-#define GIT_COMMIT_DATE "2023-09-14T05:47:17+00:00"
-#define BUILD_DATE "2023-09-14T05:47:59+00:00"
-#endif
