0eabb5520d9498f995f3d871e4fa216212b98ae9
Diana Actions <diana-actions@flyingfortressit.ca>
2023-09-17 16:05:16 +0000

    ACTION: Updating unisim headers and binaries

--- unisim/unisim/src/include/__version.hpp
+++ unisim/unisim/src/include/__version.hpp
@@ -1,7 +1,7 @@
 #ifndef __VERSION_HPP
 #define __VERSION_HPP
-#define GIT_VERSION "7d65fa84"
-#define GIT_BRANCH "54/change-default-to-realtime-physics"
-#define GIT_COMMIT_DATE "2023-09-14T05:47:17+00:00"
-#define BUILD_DATE "2023-09-14T05:47:59+00:00"
+#define GIT_VERSION "1ed6b400"
+#define GIT_BRANCH "56/explore-codegen-for-c-messaging-and-boostpython/build-unisim"
+#define GIT_COMMIT_DATE "2023-09-17T02:59:20+00:00"
+#define BUILD_DATE "2023-09-17T16:04:24+00:00"
 #endif
